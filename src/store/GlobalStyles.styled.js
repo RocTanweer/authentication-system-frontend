feaf8e8de747cbba08c33c97d29ca694f766db2b
RocTanweer <mohammadtanweer769@gmail.com>
2021-11-16 20:12:37 +0530

    ADD: made flex styled || Styled Link

--- src/store/GlobalStyles.styled.js
+++ src/store/GlobalStyles.styled.js
@@ -1,6 +1,26 @@
 import { createGlobalStyle } from "styled-components";
+import styled, { css } from "styled-components";
+import { rem } from "../utilities/functions";
+import { Link } from "react-router-dom";
 
-export default createGlobalStyle`
+export const flex = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SrOnly = styled.h1`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  border: 0;
+`;
+
+export const GlobalStyles = createGlobalStyle`
     html{
         font-size:100%;
     }
@@ -20,7 +40,24 @@ export default createGlobalStyle`
 
     #root {
         width: 100%;
-        height: 100%;
+        min-height: 100vh;
         background-color: #ffffff;
     }
+
+    main{
+        ${flex}
+        width: 100%;
+        min-height: 100vh;
+    }
+    p{
+        font-weight: 400;
+        line-height: ${rem(21.79)};
+        color: #333333;
+        letter-spacing: 0.8px;
+    }
+`;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #2f80ed;
 `;
