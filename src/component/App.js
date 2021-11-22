0a8c04547a064f4a5b6738e935224512dc933a22
RocTanweer <mohammadtanweer769@gmail.com>
2021-11-22 14:21:23 +0530

    ADD: added logic for when user reloads, they will get new accesstoken which will get saved in app state

--- src/component/App.js
+++ src/component/App.js
@@ -3,8 +3,35 @@ import Signup from "../pages/signup/Signup";
 import Profile from "../pages/profile/Profile";
 import { GlobalStyles } from "../store/GlobalStyles.styled";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  getAccessToken,
+  useGlobalContext,
+  actions,
+} from "../store/GlobalContextProvider";
+import { useEffect } from "react";
+import { existInLS, fetchFromLS } from "../utilities/functions";
 
 function App() {
+  const { dispatch } = useGlobalContext();
+
+  useEffect(() => {
+    const getnewAT = async () => {
+      try {
+        if (existInLS("userInfo")) {
+          const { refreshToken, userId } = fetchFromLS("userInfo");
+          const newAccessToken = await getAccessToken(userId, refreshToken);
+          dispatch({
+            type: actions.NEW_AT,
+            payload: { accessToken: newAccessToken },
+          });
+        }
+      } catch (err) {
+        //td show some notification if promise got rejected
+        console.error(err);
+      }
+    };
+    getnewAT();
+  }, []);
   return (
     <main>
       <BrowserRouter>
