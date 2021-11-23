8de9a02569c33b2c23812bb1b6205a48f5e935ff
RocTanweer <mohammadtanweer769@gmail.com>
2021-11-23 11:00:58 +0530

    FIX: transfered functionality of getting AT from App to Profile

--- src/component/App.js
+++ src/component/App.js
@@ -3,20 +3,8 @@ import Signup from "../pages/signup/Signup";
 import Profile from "../pages/profile/Profile";
 import { GlobalStyles } from "../store/GlobalStyles.styled";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import {
-  getAccessTokenToState,
-  useGlobalContext,
-} from "../store/GlobalContextProvider";
-import { useEffect } from "react";
-import { runFuncInInterval } from "../utilities/functions";
 
 function App() {
-  const { dispatch } = useGlobalContext();
-
-  useEffect(() => {
-    getAccessTokenToState(dispatch);
-    runFuncInInterval(getAccessTokenToState, 60, dispatch);
-  }, []);
   return (
     <main>
       <BrowserRouter>
--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -3,8 +3,33 @@ import { StyledProfile } from "./Profile.styled";
 import Nav from "../../component/nav/Nav";
 import { Navigate } from "react-router-dom";
 import { existInLS } from "../../utilities/functions";
+import { useEffect, runFuncInInterval } from "react";
+import {
+  useGlobalContext,
+  getUserProfileDetails,
+  getAccessTokenToState,
+} from "../../store/GlobalContextProvider";
 
 function Profile() {
+  const { state, dispatch } = useGlobalContext();
+
+  useEffect(() => {
+    const getUserDetails = async () => {
+      try {
+        console.log("above gat");
+        getAccessTokenToState(dispatch);
+        console.log("below gat");
+        runFuncInInterval(getAccessTokenToState, 60, dispatch);
+        console.log("above gupd");
+        await getUserProfileDetails(dispatch, state);
+        console.log("below gat");
+      } catch (err) {
+        console.error(err.response);
+      }
+    };
+    getUserDetails();
+  }, []);
+
   return (
     <StyledProfile>
       {!existInLS("userInfo") && <Navigate to="/login" replace="true" />}
